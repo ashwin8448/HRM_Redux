15e88555ba609efc4301419419880bae273e4c35
seramg <leelasera19@gmail.com>
2023-12-12 17:28:58 +0530

    [add]: error page and implemented error boundary

--- src/core/interfaces/interface.ts
+++ src/core/interfaces/interface.ts
@@ -2,3 +2,12 @@ export interface ISkill {
     id: number;
     skill: string;
 }
+
+export interface IErrorBoundaryProps {
+    children: React.ReactNode;
+}
+
+export interface IErrorState {
+    hasError: boolean;
+    error: { message: string };
+}
