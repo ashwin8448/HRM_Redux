abe67e208ed57c44708484cb46d9ae530a5d783c
Ashwin Nair <ashwin.nair@qburst.com>
2023-12-12 17:46:36 +0530

    [edit]: View employee page

--- src/core/interfaces/interface.ts
+++ src/core/interfaces/interface.ts
@@ -2,3 +2,18 @@ export interface ISkill {
     id: number;
     skill: string;
 }
+
+export interface IDepartment {
+    id: number;
+    department: string;
+}
+
+export interface IRole {
+    id: number;
+    role: string;
+}
+
+export interface IEmpMode {
+    id: number;
+    mode: string;
+}
