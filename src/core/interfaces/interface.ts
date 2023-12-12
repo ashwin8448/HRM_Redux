d4a7bdbffa0acf0c0d0251739cc2db5ba6bc327f
seramg <leelasera19@gmail.com>
2023-12-12 18:15:54 +0530

    [add]: filter select

--- src/core/interfaces/interface.ts
+++ src/core/interfaces/interface.ts
@@ -1,6 +1,36 @@
+import { Control } from "react-hook-form";
+
+export interface IEmployee {
+    id: string,
+    firstName: string,
+    lastName: string,
+    isActive: boolean,
+    dob: string,
+    email: string,
+    phone: string,
+    designation: string,
+    salary?: string,
+    dateOfJoining: string,
+    address: string,
+    moreDetails: string,
+    role: IRole,
+    department: IDepartment,
+    emp_name: string,
+    employment_mode: string,
+    gender: string,
+    skills: ISkill[],
+}
 export interface ISkill {
     id: number;
     skill: string;
+}
+export interface IRole {
+    id: number,
+    role: string
+}
+export interface IDepartment {
+    id: number,
+    department: string
 }
 
 export interface IErrorBoundaryProps {
@@ -41,11 +71,19 @@ export interface ISelectOptionProps {
     value: string;
     label: string;
 }
+export interface ISelectDropdownProps {
+    label: string;
+    options?: ISelectOptionProps[]; //TODO: change to non nullable
+    placeholder: string;
+    isMulti?: boolean;
+    control?: Control<IEmployee, any>;
+    fieldName: keyof ITableProps;
+    value?: ISelectOptionProps | ISelectOptionProps[] | null;
+}
 
 export interface ITableProps {
     department: ISelectOptionProps | null;
-    designation: ISelectOptionProps | null;
     skills: ISelectOptionProps[] | null;
-    employment_mode: ISelectOptionProps | null;
+    role: ISelectOptionProps | null;
     search_term: string | null;
 }
