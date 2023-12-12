d2cd012accca4d449ac958b25cbe0bfc176d3f9a
seramg <leelasera19@gmail.com>
2023-12-12 15:41:58 +0530

    [add]: starter code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./pages/routes.tsx";
-import DataProvider from "./core/store/DataProvider.tsx";
-import GlobalStyle from "./core/styles/global.styled.ts";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
+import router from "./pages/routes.tsx";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <ErrorBoundary>
-        <DataProvider>
           <RouterProvider router={router} />
-        </DataProvider>
       </ErrorBoundary>
     </>
   );
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,34 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import EmployeeListing from "./EmployeeListing/EmployeeListing.tsx";
-import Form from "./EmployeeUpdate/Form.tsx";
-import EmployeeView from "./EmployeeView/EmployeeView.tsx";
-import Layout from "./Layout.tsx";
+
 import ErrorPage from "../components/ErrorPage/ErrorPage.tsx";
 
 const router = createBrowserRouter(
   [
     {
-      element: <Layout></Layout>,
+      element: <></>,
       children: [
         {
           path: "/",
-          element: <EmployeeListing />,
-        },
-        {
-          path: "edit-employee",
-          element: <Form />,
-        },
-        {
-          path: "add-employee",
-          element: <Form />,
-        },
-        {
-          path: "view-employee",
-          element: <EmployeeView />,
-        },
-        {
-          path: "/error?statusCode=:statusCode",
-          element: <ErrorPage />,
+          element: <></>,
         },
       ],
       errorElement: <ErrorPage />,
