5a6d7ba4995c88b58dc087222becf27785f328de
seraGeorge <sera.george@qburst.com>
2023-12-13 22:17:34 +0530

    [remove]: unnecessary codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
 import router from "./pages/routes.tsx";
 
 function App() {
   return (
     <>
-      <ErrorBoundary>
-          <RouterProvider router={router} />
-      </ErrorBoundary>
+      <RouterProvider router={router} />
     </>
   );
 }
