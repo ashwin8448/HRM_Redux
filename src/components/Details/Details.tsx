abd86ad9c358c40354aed78c9cfbcf99686bb499
Ashwin Nair <ashwin.nair@qburst.com>
2023-12-15 13:47:59 +0530

    [edit]: Active chip and details component

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -3,27 +3,30 @@ import SkillsChip from "../Skills/SkillsChip.tsx";
 import DetailsWrapper from "./details.ts";
 
 const DetailsSection = ({
-    icon,
-    title,
-    content,
+  icon,
+  title,
+  content,
+  newline,
 }: {
-    icon: string;
-    title: string;
-    content: string | ISkill[];
+  icon: string;
+  title?: string;
+  content: string | ISkill[];
+  matches?: boolean;
+  newline?: boolean;
 }) => {
-    return (
-        <DetailsWrapper className="common-flex">
-            <div className="heading overflow-ellipsis">
-                <span className="material-symbols-outlined ">{icon}</span>
-                <p className="title">{title}</p>
-            </div>
-            {typeof content === "string" ? (
-                <p className="content overflow-ellipsis">{content}</p>
-            ) : (
-                <SkillsChip className="content" skills={content} />
-            )}
-        </DetailsWrapper>
-    );
+  return (
+    <DetailsWrapper $newline={newline} $skill={typeof content != "string"}>
+      <div className="description">
+        <span className="material-symbols-outlined ">{icon}</span>
+        {title && <span className="title">{title}</span>}
+      </div>
+      {typeof content === "string" ? (
+        <p className="content overflow-ellipsis">{content}</p>
+      ) : (
+        <SkillsChip className="content" skills={content} />
+      )}
+    </DetailsWrapper>
+  );
 };
 
 export default DetailsSection;
