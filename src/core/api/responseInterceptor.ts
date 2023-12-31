6f9321e9db5168540d280125726ff6152c1e8287
AshwinNair <ashbe404@gmail.com>
2023-12-31 18:31:02 +0530

    [FIX]: Navigation issue and refresh token issue

--- src/core/api/responseInterceptor.ts
+++ src/core/api/responseInterceptor.ts
@@ -1,6 +1,7 @@
-import { AxiosError, AxiosResponse } from 'axios';
-import { getNewRefreshToken, setCookie } from '../../utils/helper';
-import { API } from '.';
+import { AxiosError, AxiosResponse } from "axios";
+import { getNewRefreshToken, setCookie } from "../../utils/helper";
+import { API } from ".";
+import { apiURL } from "../config/constants";
 
 export enum HTTP_STATUS {
   SUCCESS = 200,
@@ -24,18 +25,19 @@ export async function onResponseError(error: AxiosError): Promise<AxiosError> {
       error.response?.status === HTTP_STATUS.FORBIDDEN ||
       error.response?.status === HTTP_STATUS.NOT_FOUND ||
       error.response?.status === HTTP_STATUS.BAD_REQUEST) &&
-    window.location.pathname !== '/error'
+    window.location.pathname !== "/error"
   ) {
     window.location.href = `/error?statusCode=${error.response?.status}`;
     return Promise.reject(error.response.data as IErrorResponse);
   } else if (
     error.response?.status === HTTP_STATUS.UNAUTHORIZED &&
-    window.location.pathname !== '/error'
+    window.location.pathname !== "/error" &&
+    config!.url != apiURL.authRenew
   ) {
     const refreshResponse = await getNewRefreshToken();
     if (refreshResponse) {
-      setCookie('accessToken', refreshResponse.access_token);
-      setCookie('refreshToken', refreshResponse.refresh_token);
+      setCookie("accessToken", refreshResponse.access_token);
+      setCookie("refreshToken", refreshResponse.refresh_token);
       return API(config!);
     } else {
       return Promise.reject(error.response.data as IErrorResponse);
