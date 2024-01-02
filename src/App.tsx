19a62d32c21d5e4298b4597caf00313cebf454c5
AshwinNair <ashbe404@gmail.com>
2024-01-02 17:15:00 +0530

    [edit]: Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import store from "./core/store/configureStore.ts";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import router from "./pages/routes.tsx";
+
 function App() {
   return (
     <>
