c6b759ab23619eec77d457cf27ee1a7234e51a08
seramg <leelasera19@gmail.com>
2024-01-03 17:33:36 +0530

    [add]:Minor change in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: '/',
-  }
+  };
 
-  if (command !== 'serve') {
-    config.base = '/HRM_Redux_Auth/'
-  }
-
-  return config
-})
+  return config;
+});
